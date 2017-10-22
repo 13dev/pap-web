7b158916ad2da238c7c307b278c77133b2f6b941
13dev <qwerty124563@gmail.com>
2017-10-22 19:20:01 +0100

    Add Auth,Guest Middleware

---
+++ app/Middleware/AuthMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Middleware;
+
+/**
+* Protect routes
+*/
+class AuthMiddleware extends Middleware
+{
+	
+	public function __invoke($request, $response, $next)
+	{
+		if(!$this->container->auth->check())
+		{
+			$this->container->flash->addMessage('nav-error', 'Não podes aceder a esta área.');
+			return $response->withRedirect($this->container->router->pathFor('home'));
+		}
+		
+		$response = $next($request, $response);
+		return $response;
+	}
+}
--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Middleware\AuthMiddleware;
+use App\Middleware\GuestMiddleware;
 /**
  * Routes
  */
@@ -7,19 +8,27 @@
 $app->get('/', 'HomeController:index')->setName('home');
 
 //Auth
-$app->group('/auth', function () {
-	//signup
-	$this->get('/signup', 'AuthController:getSignUp')->setName('auth.signup');
-	$this->post('/signup', 'AuthController:postSignUp');
+$app->group('/auth', function () use($container) {
 
-	//signin
-	$this->get('/signin', 'AuthController:getSignIn')->setName('auth.signin');
-	$this->post('/signin', 'AuthController:postSignIn');
+	$this->group('', function () {
+		//signup
+		$this->get('/signup', 'AuthController:getSignUp')->setName('auth.signup');
+		$this->post('/signup', 'AuthController:postSignUp');
 
-	//signout
-	$this->get('/signout', 'AuthController:getSignOut')->setName('auth.signout');
+		//signin
+		$this->get('/signin', 'AuthController:getSignIn')->setName('auth.signin');
+		$this->post('/signin', 'AuthController:postSignIn');
 
-	//Change password
-	$this->get('/password/change', 'PasswordController:getChangePassword')->setName('auth.password.change');
-	$this->post('/password/change', 'PasswordController:postChangePassword');
+	})->add(new GuestMiddleware($container));
+
+	$this->group('', function () {
+		//signout
+		$this->get('/signout', 'AuthController:getSignOut')->setName('auth.signout');
+
+		//Change password
+		$this->get('/password/change', 'PasswordController:getChangePassword')->setName('auth.password.change');
+		$this->post('/password/change', 'PasswordController:postChangePassword');
+		
+	})->add(new AuthMiddleware($container));
+
 });
