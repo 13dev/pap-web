cb08f5e3a40adaa17ecf3ffa2bd7a9864121874c
13dev <qwerty124563@gmail.com>
2017-12-01 20:24:37 +0000

    fix config

--- app/config.php
+++ app/config.php
@@ -18,8 +18,9 @@ $config = [
             'charset'       => 'utf8',
             'collation'     => 'utf8_unicode_ci',
             'prefix'        => '',
-		    'translations_path' => __DIR__ . '/Translations/' // path to the translation files
-            ]
+
+        ],
+        'translations_path' => __DIR__ . '/Translations/' // path to the translation files
     ],
 
 ];
